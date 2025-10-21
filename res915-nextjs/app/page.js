796d2a1b8d360e32ec987af5bc3470d14b6a30e2
jiacqventures <jiacqventures@gmail.com>
2025-10-21 13:30:01 -0600

    Update page.js

--- res915-nextjs/app/page.js
+++ res915-nextjs/app/page.js
@@ -1,1 +1,9 @@
+'use client';
+export const dynamic = 'force-dynamic';
+
+import RealEstateLanding from '../components/RealEstateLanding';
+
+export default function Page() {
+  return <RealEstateLanding />;
+}
 
