4c5228e0d508cd7048a75c09d50c024d776c3bc5
jiacqventures <jiacqventures@gmail.com>
2025-10-21 13:11:26 -0600

    Update layout.js

--- res915-nextjs/app/layout.js
+++ res915-nextjs/app/layout.js
@@ -1,1 +1,15 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'res915',
+  description: 'Real Estate Solutions 915',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
 
