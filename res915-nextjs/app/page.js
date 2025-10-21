8357565082c6d3428fffe9eb661d44b4f1840a38
jiacqventures <jiacqventures@gmail.com>
2025-10-21 13:37:29 -0600

    Update page.js

--- res915-nextjs/app/page.js
+++ res915-nextjs/app/page.js
@@ -1,9 +1,9 @@
-'use client';
-export const dynamic = 'force-dynamic';
-
-import RealEstateLanding from '../components/RealEstateLanding';
-
-export default function Page() {
-  return <RealEstateLanding />;
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ textAlign: 'center', paddingTop: '40px' }}>
+        RES915 landing page coming soon...
+      </h1>
+    </main>
+  );
 }
-
